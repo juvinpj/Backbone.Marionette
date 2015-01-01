4d13915470199679a3cc8423307333861535f9cc
juvinpj <juvinpj@gmail.com>
2015-01-01 03:36:19 -0600

    Create collections.js

---
+++ collection/collections.js
@@ -0,0 +1,26 @@
+MyApp.Categories = Backbone.Collection.extend({
+  model: MyApp.Category,
+  initialize: function(categories){
+    this.on('add', function(category){
+      if( ! category.get('category') ){
+        var error =  Error("Category must have a category name defined before being added to the collection");
+        error.name = "NoRankError";
+        throw error;
+      } 
+    }); 
+  }
+   
+});
+
+MyApp.Events = Backbone.Collection.extend({
+  model: MyApp.Event,
+  initialize: function(events){
+    this.on('add', function(event){
+      if( ! event.get('eventType') ){
+        var error =  Error("Event must have a event Type  defined before being added to the collection");
+        error.name = "NoRankError";
+        throw error;
+      }
+    });
+  }
+});
