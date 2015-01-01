6ba31132c85e1553c6429b51baa92ef72e7ad181
juvinpj <juvinpj@gmail.com>
2015-01-01 03:29:42 -0600

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'build.js',
+        dest: 'build/build.min.js'
+      }
+    },
+    sass : {
+      dist : {
+        files : {
+          'assets/css/custom.css' :  'assets/sass/custom.scss'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
