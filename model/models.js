cc7cef82ae526cec7c8a7b4a93bcec900e3ff604
juvinpj <juvinpj@gmail.com>
2015-01-01 03:34:43 -0600

    Create models.js

---
+++ model/models.js
@@ -0,0 +1,14 @@
+MyApp.Category = Backbone.Model.extend({
+  defaults: {
+    events: 0,
+    category : ''
+  }
+});
+
+MyApp.Event = Backbone.Model.extend({
+  defaults: {
+    eventName: '',
+    eventDate: '',
+    eventType:''
+  }
+});
