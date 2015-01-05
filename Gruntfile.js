cef54c600392510643cdd1f68b2fb5dc2e0333af
juvinpj <juvinpj@gmail.com>
2015-01-05 01:33:42 -0600

    added grunt browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+    dist: {
+        files: {
+          'build.js': ['app.js'],
+        }
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -22,10 +29,12 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+
+  grunt.registerTask('default', ['browserify', 'uglify', 'sass']);
 
 };
